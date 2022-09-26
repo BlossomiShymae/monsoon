e3f020278ca372854ec0657da0294b4e4188bc71
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-09-26 18:50:48 -0500

    Implement overlay functionality

--- src/monsoon.py
+++ src/monsoon.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6 import QtWidgets, QtCore, QtGui
 from constant import Embedded
+from service import TimerService
 from view import MainView
 
 def use_embedded_icon(app: QtWidgets.QApplication, b64_image):
@@ -9,6 +10,9 @@ def use_embedded_icon(app: QtWidgets.QApplication, b64_image):
   icon = QtGui.QIcon(pixmap)
   app.setWindowIcon(icon)
 
+def milliseconds_from_fps(fps: int) -> int:
+  return (1 / fps) * 1000
+
 if __name__ == "__main__":
   app = QtWidgets.QApplication([])
 
@@ -16,5 +20,9 @@ if __name__ == "__main__":
   view.show()
 
   use_embedded_icon(app, Embedded.icon())
+
+  # Refresh view based on update tick rate (20fps)
+  update_timer = TimerService(milliseconds_from_fps(20))
+  update_timer.add_slot(view.refresh)
 
   sys.exit(app.exec())
--- src/view/mainview.py
+++ src/view/mainview.py
@@ -1,12 +1,30 @@
 from PySide6 import QtWidgets, QtCore
 from constant import Monsoon
+from controller import LeagueClientController
 
 class MainView(QtWidgets.QMainWindow):
   def __init__(self):
     super().__init__()
 
+    # Set instance variables
+    self.client_controller = LeagueClientController()
+
     # Set window properties
     self.resize(Monsoon.WIDTH.value, Monsoon.HEIGHT.value)
     self.setWindowTitle(Monsoon.TITLE.value)
+    self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+    self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
     
+  @QtCore.Slot()
+  def refresh(self):
+    if (self.client_controller.is_active()):
+      (left, top, right, bottom) = self.client_controller.find()
+
+      # Calculate window dimensions
+      height = bottom - top
+      width = right - left
+
+      # Adjust view based on League client (lock onto it)
+      self.setGeometry(left, top, width, height)
+    pass
 
