99969fb0991e9f58fa31a4342a095fe04c2319f9
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-09-26 23:12:07 -0500

    Add api for DataDragon

---
+++ src/api/__init__.py
@@ -0,0 +1,1 @@
+from api.datadragon import DataDragon
---
+++ src/api/datadragon.py
@@ -0,0 +1,34 @@
+import requests
+
+class DataDragon():
+  def __init__(self):
+    self.url = "https://ddragon.leagueoflegends.com"
+    self.latest_version = self.__fetch_latest_version()
+    self.champions = self.__fetch_champions()
+  
+  def __fetch_latest_version(self):
+    req = requests.get(f"{self.url}/api/versions.json")
+
+    if req.status_code != 200:
+      raise Exception("Failed to get latest version from DataDragon")
+    
+    if len(req.json()) == 0:
+      raise Exception("Received empty versions list from DataDragon")
+
+    return req.json()[0]
+
+  def __fetch_champions(self):
+    req = requests.get(f"{self.url}/cdn/{self.latest_version}/data/en_US/champion.json")
+
+    if req.status_code != 200:
+      raise Exception("Failed to get champions from DataDragon")
+    
+    return req.json()
+  
+  def fetchByChampionId(self, champion_id):
+    data = self.champions["data"]
+    for id in data:
+      if (int(data[id]["key"]) == champion_id):
+        return data[id]
+    
+    return None
