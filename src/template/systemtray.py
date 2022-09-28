adf42f19d31b2e50ee97bc30a11712eb4534eb4b
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-09-28 15:37:12 -0500

    Add application exit functionality

--- src/template/systemtray.py
+++ src/template/systemtray.py
@@ -1,21 +1,26 @@
+import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from constant import Embedded, Monsoon
 
 class SystemTray(QtWidgets.QSystemTrayIcon):
   def __init__(self):
     super().__init__()
+    
+    # Setup system tray icon
     self.setIcon(self.__b64_to_qicon(Embedded.icon()))
     self.setToolTip(Monsoon.TITLE.value)
 
     # Set context menu
     menu = QtWidgets.QMenu()
 
+    # Setup context menu items
     title = menu.addAction(f"{Monsoon.TITLE.value}")
     title.setEnabled(False)
     menu.addSeparator()
     github = menu.addAction("GitHub")
     menu.addSeparator()
     exit = menu.addAction("Exit")
+    exit.triggered.connect(self.__exit_application)
 
     self.setContextMenu(menu)
 
@@ -25,3 +30,6 @@ class SystemTray(QtWidgets.QSystemTrayIcon):
     icon = QtGui.QIcon(pixmap)
 
     return icon
+  
+  def __exit_application(self):
+    sys.exit()
