e1d34c7d13266131e7fe8ad3459467db90db236e
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-09-28 17:33:09 -0500

    Add base64 to qpixmap function

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,8 +1,12 @@
 from PySide6 import QtCore, QtGui
 
 def b64_to_qicon(b64_image) -> QtGui.QIcon:
-    pixmap = QtGui.QPixmap()
-    pixmap.loadFromData(QtCore.QByteArray.fromBase64(b64_image))
-    icon = QtGui.QIcon(pixmap)
+    icon = QtGui.QIcon(b64_to_qpixmap(b64_image))
 
     return icon
+
+def b64_to_qpixmap(b64_image) -> QtGui.QPixmap:
+    pixmap = QtGui.QPixmap()
+    pixmap.loadFromData(QtCore.QByteArray.fromBase64(b64_image))
+
+    return pixmap
