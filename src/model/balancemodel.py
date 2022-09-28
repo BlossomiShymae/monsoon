632feeadcf94bd4370c4e7fe46b9fe71efa9b961
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-09-27 20:43:10 -0500

    Add string format methods to balance model

--- src/model/balancemodel.py
+++ src/model/balancemodel.py
@@ -2,9 +2,37 @@ from dataclasses import dataclass
 
 @dataclass
 class BalanceModel:
-  """Class for caching ARAM balance changes of a champion from LoL Fandom.
+  """Data class for caching ARAM balance changes of a champion from LoL Fandom.
   """
   champion_name: str
-  damage_dealt: int
-  damage_received: int
+  damage_dealt: str
+  damage_received: str
   other_changes: str
+
+  def __format_champion_name(self):
+    return f"{self.champion_name}"
+
+  def __format_damage_dealt(self):
+    return f"Damage dealt: {self.damage_dealt}"
+  
+  def __format_damage_received(self):
+    return f"Damage received: {self.damage_received}"
+  
+  def __format_other_changes(self):
+    return f"Other changes: {self.other_changes}"
+
+  def format(self):
+    """Return a formatted label string of balance changes for a champion.
+
+    Returns:
+        str: Formatted string representation of balance changes.
+    """
+    label = self.__format_champion_name()
+    if len(self.damage_dealt) > 0:
+      label += f"{self.__format_damage_dealt()}\n" 
+    if len(self.damage_received) > 0:
+      label += f"{self.__format_damage_received()}\n"
+    if len(self.other_changes) > 0:
+      label += self.__format_other_changes()
+    
+    return label
