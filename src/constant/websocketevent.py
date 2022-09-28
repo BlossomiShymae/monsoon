21e4f56c91325e8c45a754010d449075f4ed8b66
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-09-27 21:45:53 -0500

    Implement equality dunder method

--- src/constant/websocketevent.py
+++ src/constant/websocketevent.py
@@ -7,3 +7,16 @@ class WebSocketEvent(Enum):
   DELETE = "Delete"
   UPDATE = "Update"
   
+  def __eq__(self, __o: object) -> bool:
+    if self.__class__ is __o.__class__:
+      return self == __o
+    try:
+      return self.value == __o.value
+    except:
+      pass
+    try:
+      if isinstance(__o, str):
+        return self.value == __o
+    except:
+      pass
+    return NotImplemented
