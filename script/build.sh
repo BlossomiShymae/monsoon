fd86aacfcad9f2792b66f8ae64fb2422987bcf54
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-09-29 14:49:34 -0500

    Add build for executable without console

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 # Build the application
-pyinstaller src/monsoon.py --onefile --hidden-import "win32api" --icon "monsoon.ico"
+pyinstaller src/monsoon.py --onefile --hidden-import "win32api" --icon "monsoon.ico" -n "monsoon-debug-console"
+pyinstaller src/monsoon.py --onefile --noconsole --hidden-import "win32api" --icon "monsoon.ico"
 
 # Archive and compress redistributables
 git archive -o dist/latest.zip HEAD
