86d39a89f9ec8f2d2e92b4e4399a48e4c1747323
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-10-04 15:29:52 -0500

    Add disclaimer

--- src/constant/monsoon.py
+++ src/constant/monsoon.py
@@ -2,5 +2,6 @@ from enum import Enum
 
 class Monsoon(Enum):
   TITLE = "Monsoon"
+  LEGAL = "Monsoon isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc."
   WIDTH = 1280
   HEIGHT = 720
