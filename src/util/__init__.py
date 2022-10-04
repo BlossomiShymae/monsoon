789f453255f58d40918e85a6d4342f2ff6758e7d
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-10-04 18:02:49 -0500

    Add static layout factory

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,4 +1,5 @@
-from PySide6 import QtCore, QtGui
+from abc import abstractmethod
+from PySide6 import QtCore, QtGui, QtWidgets
 
 def b64_to_qicon(b64_image) -> QtGui.QIcon:
     icon = QtGui.QIcon(b64_to_qpixmap(b64_image))
@@ -10,3 +11,34 @@ def b64_to_qpixmap(b64_image) -> QtGui.QPixmap:
     pixmap.loadFromData(QtCore.QByteArray.fromBase64(b64_image))
 
     return pixmap
+
+class LayoutFactory():
+    """Represents a static factory that creates layout widgets in tuple formats.
+    """
+    @abstractmethod
+    def create_grid(self):
+        grid = QtWidgets.QGroupBox()
+        grid_layout = QtWidgets.QGridLayout()
+        grid_layout.setSpacing(0)
+        grid.setLayout(grid_layout)
+
+        return (grid, grid_layout)
+
+    @abstractmethod
+    def create_horizontal():
+        hbox = QtWidgets.QGroupBox()
+        hbox_layout = QtWidgets.QHBoxLayout()
+        hbox_layout.setSpacing(0)
+        hbox.setLayout(hbox_layout)
+
+        return (hbox, hbox_layout)
+    
+    @abstractmethod
+    def create_vertical(self):
+        vbox = QtWidgets.QGroupBox()
+        vbox_layout = QtWidgets.QVBoxLayout()
+        vbox_layout.setSpacing(0)
+        vbox.setLayout(vbox_layout)
+
+        return (vbox, vbox_layout)
+
