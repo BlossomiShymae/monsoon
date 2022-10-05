68aa5fafdabc7cc2b0b8315bce3c0680aa453b38
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-10-04 19:54:01 -0500

    Add action to open about view

--- src/template/systemtray.py
+++ src/template/systemtray.py
@@ -3,6 +3,7 @@ import webbrowser
 from PySide6 import QtWidgets
 from constant import Embedded, Monsoon
 from util import b64_to_qicon
+from view import AboutView
 
 class SystemTray(QtWidgets.QSystemTrayIcon):
   def __init__(self):
@@ -11,6 +12,7 @@ class SystemTray(QtWidgets.QSystemTrayIcon):
     # Setup system tray icon
     self.setIcon(b64_to_qicon(Embedded.icon()))
     self.setToolTip(Monsoon.TITLE.value)
+    self.about_dialog = AboutView()
 
     # Set context menu
     menu = QtWidgets.QMenu()
@@ -23,11 +25,16 @@ class SystemTray(QtWidgets.QSystemTrayIcon):
     github.triggered.connect(
       lambda: self.__open_web_link__("https://github.com/MissUwuieTime/monsoon")
     )
+    about = menu.addAction("About")
+    about.triggered.connect(self.__open_about__)
     menu.addSeparator()
     exit = menu.addAction("Exit")
     exit.triggered.connect(self.__exit_application__)
 
     self.setContextMenu(menu)
+  
+  def __open_about__(self):
+    self.about_dialog.show()
 
   def __open_web_link__(self, url: str):
     webbrowser.open(url)
