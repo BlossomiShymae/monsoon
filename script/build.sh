5d16b78715bda4b520990f27e826b3ba4cbb43e1
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-10-05 10:41:37 -0500

    Update build script

--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,29 @@
 #! /bin/bash
 
-# Build the application
-pyinstaller src/monsoon.py --onefile --hidden-import "win32api" --icon "monsoon.ico" -n "monsoon-debug-console"
-pyinstaller src/monsoon.py --onefile --noconsole --hidden-import "win32api" --icon "monsoon.ico"
+version="0.0.0"
+exe_name="monsoon"
+debug_exe_name="monsoon-debug-console"
 
-# Archive and compress redistributables
+# Parse canonical version from Python source file
+parse_version() {
+    version=$(awk '/VERSION/' ./src/constant/monsoon.py | cut -d '"' -f2)
+}
+
+# Setup names for the distributed executables
+setup_executable_names() {
+    exe_name="${exe_name}-${version}"
+    debug_exe_name="${debug_exe_name}-${version}"
+}
+
+# Build the application
+build() {
+    pyinstaller src/monsoon.py --onefile --hidden-import "win32api" --icon "monsoon.ico" -n "$debug_exe_name"
+    pyinstaller src/monsoon.py --onefile --noconsole --hidden-import "win32api" --icon "monsoon.ico" -n "$exe_name"
+}
+
+parse_version
+setup_executable_names
+build
+
+# Archive and compress source code
 git archive -o dist/latest.zip HEAD
