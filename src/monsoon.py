b36d6ad987317ead71ea59ed2ba4886a97f12598
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-10-09 15:37:46 -0500

    Refactor to use executor pattern

--- src/monsoon.py
+++ src/monsoon.py
@@ -1,17 +1,13 @@
 from constant import Embedded, Stylesheet
 from controller import EventDataController, LeagueClientController
-from service import TimerService
+from service import ExecutorService, TimerService
 from template import SystemTray
 from util import b64_to_qicon
 from view import MainView
 
 import asyncio
-import json
 import logging
-import willump
 from PySide6 import QtWidgets
-from threading import Thread
-from time import sleep
 
 def milliseconds_from_fps(fps: int) -> int:
   """Calculate milliseconds from the rate of frames per second.
@@ -24,47 +20,35 @@ def milliseconds_from_fps(fps: int) -> int:
   """
   return (1 / fps) * 1000
 
-async def data_callback(data, controller: EventDataController):
-  """Pass WebSocket event data to the event data controller.
+def create_app() -> QtWidgets.QApplication:
+  """Create our main Qt application with settings applied.
 
-  Args:
-      data (object): League client event data
-      controller (EventDataController): Controller for WebSocket event data
+  Returns:
+      QtWidgets.QApplication: Main application
   """
-  print(json.dumps(data, indent=4, sort_keys=True))
-  controller.events_queue.append(data)
-
-async def setup_client_connection(controller: EventDataController):
-  """Setup the League client connection and subscribe to WebSocket.
-
-  Args:
-      controller (EventDataController): Event data controller
-  """
-  global wllp
-  wllp = await willump.start()
-  
-  subscription = await wllp.subscribe(
-    "OnJsonApiEvent_lol-champ-select_v1_session",
-     default_handler=lambda data: data_callback(data, controller)
-  )
-
-async def main():
-  """Asynchronously run the main application.
-  """
-  logging.basicConfig(level=logging.DEBUG)
-
-  # Setup application
   app = QtWidgets.QApplication([])
   app.setStyleSheet(Stylesheet.value())
   app.setWindowIcon(b64_to_qicon(Embedded.icon()))
 
-  # Setup system tray icon
+  return app
+
+def create_tray() -> SystemTray:
+  """Create our system tray with actions.
+
+  Returns:
+      SystemTray: System tray
+  """
   tray = SystemTray()
   tray.show()
 
-  # Setup and pass controllers using dependency injection
-  event_data_controller = EventDataController()
-  client_controller = LeagueClientController()
+  return tray
+
+async def main(
+  client_controller: LeagueClientController,
+  event_data_controller: EventDataController,
+  executor: ExecutorService):
+  """Asynchronously run the main application.
+  """
   view = MainView(
     event_data_controller=event_data_controller,
     client_controller=client_controller
@@ -74,60 +58,30 @@ async def main():
   update_timer = TimerService(milliseconds_from_fps(20))
   update_timer.add_slot(view.refresh)
 
-  # Poll long-running operation for client process using another thread
-  def poll_task():
-    while True:
-      global stop_threads_flag
-      if stop_threads_flag == True:
-        break
-      sleep(5)
-      client_controller.process()
-
-  poll_process_thread = Thread(target=poll_task)
-  poll_process_thread.start()
-
-  async def exec_loop():
-    """ Execute the event loop that processes both Qt and asyncio loops.
-    """
-    global wllp
-    is_locked = False
-    is_wllp_running = False
-    while True:
-      if not is_locked:
-        if not client_controller.is_active():
-          is_locked = True
-          if wllp != None:
-            is_wllp_running = False
-            asyncio.ensure_future(wllp.close())
-
-      
-      if client_controller.is_active():
-        is_locked = False
-        if not is_wllp_running:
-          is_wllp_running = True
-
-          # Sleep for five seconds to ensure that the WebSocket subscription works. u.u
-          # This should be handled by `willump` but we have to bandaid fix this for now. :c
-          # Submit a pull request if a better alternative is found.
-          sleep(5)
-          asyncio.ensure_future(setup_client_connection(event_data_controller))
-
-      app.processEvents()
-      await asyncio.sleep(0)
-
-  await exec_loop()
-
+  await executor.exec_event_loop()
 
 if __name__ == "__main__":
-  global wllp
-  global stop_threads_flag
-  wllp = None
-  stop_threads_flag = False
+  logging.basicConfig(level=logging.DEBUG)
   loop = asyncio.get_event_loop()
-
+  app = create_app()
+  tray = create_tray()
+  client_controller = LeagueClientController()
+  event_data_controller = EventDataController()
+  executor = ExecutorService(
+    app=app, 
+    client_controller=client_controller,
+    event_data_controller=event_data_controller,
+    ui_event_loop=loop
+    )
   try:
-    loop.run_until_complete(main())
+    loop.run_until_complete(
+      main(
+        client_controller=client_controller,
+        event_data_controller=event_data_controller,
+        executor=executor
+      )
+    )
   except KeyboardInterrupt:
-    stop_threads_flag = True
-    loop.run_until_complete(wllp.close())
+    executor.is_program_exiting = True
+    loop.run_until_complete(executor._kill_willump())
     print()
