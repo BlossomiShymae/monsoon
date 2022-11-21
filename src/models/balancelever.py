9f7c0d3c93d8e01fd510e299da3d2a4dfeb67b73
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-11-20 20:50:36 -0600

    Add format functions for models

--- src/models/balancelever.py
+++ src/models/balancelever.py
@@ -8,3 +8,19 @@ class BalanceLever:
   name: str
   # The modifier a balance lever applies (0.8, 1.1...)
   modifier: float
+
+  def _format_name(self) -> str:
+    return self.name.replace("_", " ").title()
+  
+  def _format_modifier(self) -> str:
+    percentage = int((self.modifier - 1.00) * 100)
+    sign = "+" if percentage >= 0 else "-"
+    return f"{sign} {percentage}"
+  
+  def format(self) -> str:
+    """Return a formatted string of a balance lever. ("Dmg Dealt: -20%")
+
+    Returns:
+        str: Formatted string reperesentation of a balance lever.
+    """
+    return f"{self._format_name()}: {self._format_modifier()}"
--- src/models/dynamicbalancemodel.py
+++ src/models/dynamicbalancemodel.py
@@ -9,3 +9,16 @@ class DynamicBalanceModel:
   """
   champion_name: str
   balance_levers: List[BalanceLever]
+
+  def _format_champion_name(self) -> str:
+    return f"{self.champion_name}"
+  
+  def format(self):
+    """Return a formatted label string of dynamic balance changes for champion.
+
+    Returns:
+        str: Formatted string representation of dynamic balance changes.
+    """
+    label = self._format_champion_name()
+    for balance_lever in self.balance_levers:
+      label += f"\n{balance_lever.format()}"
