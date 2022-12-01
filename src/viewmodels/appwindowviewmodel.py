899d4551ee31f3e515b65a22129996dfa0a2a59d
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-12-01 05:10:29 -0600

    Add properties and setters for data binding

--- src/viewmodels/appwindowviewmodel.py
+++ src/viewmodels/appwindowviewmodel.py
@@ -4,6 +4,7 @@ if TYPE_CHECKING:
   from services import WorkerService
 
 from constants import Embedded, Monsoon
+from models import DynamicBalanceModel
 from services import Workers
 from utils import EventHandler, b64_to_qpixmap
 
@@ -11,7 +12,7 @@ from PySide6 import QtCore
 from dependency_injector.wiring import Provide, inject
 
 
-class AppWindowViewModel():
+class AppWindowViewModel(object):
   @inject
   def __init__(
     self,
@@ -24,9 +25,30 @@ class AppWindowViewModel():
     self.wordmark_qpixmap = b64_to_qpixmap(Embedded.wordmark())
     self.wordmark_qpixmap.scaled(self.wordmark_qpixmap.size(), QtCore.Qt.KeepAspectRatio)
 
+    self._available_champion_dynamic_balances = []
+    self._team_champion_dynamic_balances = []
+
     self.property_changed = EventHandler()
 
     # Start worker threads
     lockfile_watcher_worker = worker_service.get(Workers.LOCKFILE_WATCHER)
     lockfile_watcher_worker.start()
+  
+  @property
+  def available_champion_dynamic_balances(self):
+    return self._available_champion_dynamic_balances
+  
+  @available_champion_dynamic_balances.setter
+  def available_champion_dynamic_balances(self, value):
+    self._available_champion_dynamic_balances = value
+    self.property_changed.invoke(self, None)
+  
+  @property
+  def team_champion_dynamic_balances(self):
+    return self._team_champion_dynamic_balances
+  
+  @team_champion_dynamic_balances.setter
+  def team_champion_dynamic_balances(self, value):
+    self._team_champion_dynamic_balances = value
+    self.property_changed.invoke(self, None)
     
