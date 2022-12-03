db8d287024f3595e830e246ed7cabcd70ddb7220
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-12-02 20:35:40 -0600

    Add api service

---
+++ src/services/apiservice.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+  pass
+
+from apis import DataDragon, LolFandom
+
+
+class ApiService():
+  def __init__(
+    self
+  ):
+    self.data_dragon = DataDragon()
+    self.lol_fandom = LolFandom()
