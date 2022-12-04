6303ace7ea95e2e998f34d8900d6311505d875cf
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-12-03 18:21:12 -0600

    Update about view

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,5 +1,5 @@
+from .widgets.qimage import *
+from .widgets.qchampiontemplate import *
 from .aboutwindowview import *
 from .appwindowview import *
 from .systemtray import *
-from .widgets.qchampiontemplate import *
-from .widgets.qimage import *
--- src/views/aboutwindowview.py
+++ src/views/aboutwindowview.py
@@ -4,7 +4,8 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from src.viewmodels import AboutWindowViewModel
-from src.utils import LayoutFactory
+from src.utils import QtContainerFactory, QtContainerLayouts
+from src.views import QImage
 
 from PySide6 import QtWidgets, QtCore
 from dependency_injector.wiring import Provide, inject
@@ -23,36 +24,39 @@ class AboutWindowView(QtWidgets.QMainWindow):
         self.setObjectName(self.viewmodel.object_name)
 
         # Set instance variables
-        (self.vbox, self.vbox_layout) = LayoutFactory.create_vertical()
-        (self.header, self.header_layout) = LayoutFactory.create_horizontal()
+        self.about_vbox = QtContainerFactory.create(QtContainerLayouts.VERTICAL)
+        self.header_box = QtContainerFactory.create(QtContainerLayouts.STACKED)
+        self.content_vbox = QtContainerFactory.create(QtContainerLayouts.VERTICAL)
         self.ok_button = QtWidgets.QPushButton("Ok")
         self.ok_button.clicked.connect(self._hide_window)
 
         # Set header layout
-        wordmark_label = QtWidgets.QLabel("")
-        wordmark_label.setPixmap(self.viewmodel.wordmark_pixmap)
-        self.header_layout.addWidget(wordmark_label);
-        self.header_layout.setAlignment(QtCore.Qt.AlignCenter)
-        self.vbox_layout.addWidget(self.header)
+        self.header_box.layout.addWidget(QImage(self.viewmodel.wordmark_pixmap))
+        self.header_box.container.setMaximumHeight(100)
+        self.about_vbox.layout.addWidget(self.header_box.container)
 
         separator = QtWidgets.QFrame()
         separator.setFrameShape(QtWidgets.QFrame.HLine)
         separator.setFrameShadow(QtWidgets.QFrame.Sunken)
-        self.vbox_layout.addWidget(separator)
+        self.about_vbox.layout.addWidget(separator)
 
         # Set information labels
         for label in self.viewmodel.labels:
-            self.vbox_layout.addWidget(QtWidgets.QLabel(label))
+            self.content_vbox.layout.addWidget(QtWidgets.QLabel(label))
 
         disclaimer = QtWidgets.QLabel(self.viewmodel.disclaimer)
         disclaimer.setWordWrap(True)
         disclaimer.setContentsMargins(0, 16, 0, 16)
-        self.vbox_layout.addWidget(disclaimer)
-        self.vbox_layout.addWidget(self.ok_button)
+        disclaimer.setMinimumHeight(150)
+        disclaimer.setAlignment(QtCore.Qt.AlignTop)
+        self.content_vbox.layout.addWidget(disclaimer)
+        self.content_vbox.layout.setAlignment(QtCore.Qt.AlignTop)
 
-        self.vbox_layout.setSpacing(2)
-        self.setFixedSize(self.vbox_layout.sizeHint())
-        self.setCentralWidget(self.vbox)
+        self.about_vbox.layout.addWidget(self.content_vbox.container)
+        self.about_vbox.layout.addWidget(self.ok_button)
+        self.about_vbox.container.setContentsMargins(16, 16, 16, 16)
+        self.setFixedSize(self.about_vbox.layout.sizeHint())
+        self.setCentralWidget(self.about_vbox.container)
 
     def _hide_window(self):
         self.hide()
